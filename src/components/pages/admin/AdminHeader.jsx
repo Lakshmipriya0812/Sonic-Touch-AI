005868799eb94652d78565f22048c5e4455df6d2
lp <lakshmipriyarajesh0812@gmail.com>
2025-03-11 18:25:41 -0400

    Admin Issues Fixed

---
+++ src/components/pages/admin/AdminHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminHeader = () => {
+  return (
+    <header className="bg-grey-500 text-white p-4">
+      <h1 className="text-xl">Admin Panel</h1>
+    </header>
+  );
+};
+
+export default AdminHeader;
